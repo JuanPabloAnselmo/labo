fc4a612776b53bb2f4b4fa5415dd1191bb30f3b8
JuanPabloAnselmo <jpanselmo@hotmail.com>
2023-09-16 15:43:01 -0300

    canarios

--- src/workflow-inicial/601_RUN_correr_workflow_02.r
+++ src/workflow-inicial/601_RUN_correr_workflow_02.r
@@ -13,7 +13,7 @@ options(error = function() {
 source("~/labo2023ba/src/workflow-inicial/611_CA_reparar_dataset_01.r")
 source("~/labo2023ba/src/workflow-inicial/621_DR_corregir_drifting_01.r")
 source("~/labo2023ba/src/workflow-inicial/631_FE_historia_02_sc.r")
-source("~/labo2023ba/src/workflow-inicial/641_TS_training_strategy_01.r")
+source("~/labo2023ba/src/workflow-inicial/641_TS_training_strategy_02_B.r")
 
 # ultimos pasos, muy lentos
 source("~/labo2023ba/src/workflow-inicial/651_HT_lightgbm_01.r")
