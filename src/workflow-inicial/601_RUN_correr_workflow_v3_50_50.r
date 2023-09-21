50813fdd3aa7c20f18236ca991c8de661332ba5c
JuanPabloAnselmo <jpanselmo@hotmail.com>
2023-09-21 17:28:56 -0300

    Odio los canarios

--- src/workflow-inicial/601_RUN_correr_workflow_v3_50_50.r
+++ src/workflow-inicial/601_RUN_correr_workflow_v3_50_50.r
@@ -12,9 +12,9 @@ options(error = function() {
 # primeros pasos, relativamente rapidos
 source("~/labo2023ba/src/workflow-inicial/611_CA_reparar_dataset_v3.r")
 source("~/labo2023ba/src/workflow-inicial/621_DR_corregir_drifting_v3.r")
-source("~/labo2023ba/src/workflow-inicial/631_FE_historia_01.r")
-source("~/labo2023ba/src/workflow-inicial/641_TS_training_strategy_01.r")
+source("~/labo2023ba/src/workflow-inicial/631_FE_historia_v3_50_50.r")
+source("~/labo2023ba/src/workflow-inicial/641_TS_training_strategy_v3.r")
 
 # ultimos pasos, muy lentos
-source("~/labo2023ba/src/workflow-inicial/651_HT_lightgbm_01.r")
-source("~/labo2023ba/src/workflow-inicial/661_ZZ_final_01.r")
+source("~/labo2023ba/src/workflow-inicial/651_HT_lightgbm_v3.r")
+source("~/labo2023ba/src/workflow-inicial/661_ZZ_final_v3_50_50.r")
